2f3c6aaab2d0e14608f12e11e504110fdcae0e8f
annanas-code <57849804+annanas-code@users.noreply.github.com>
2019-11-16 17:49:19 -0800

    no more node!

--- server.js
+++
@@ -1,8 +0,0 @@
-const st = require('st');
-const http = require('http');
- 
-console.log('Server running on: http://localhost:1337/');
-console.log('Kill server with Ctr+C');
-http.createServer(
-  st(process.cwd())
-).listen(1337);
